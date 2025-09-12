65ac449cc5c5a3dbe0e6743ec3743f9aa3f2fec5
Guy Davidi <guydav1111@gmail.com>
2025-09-12 14:45:32 +0300

    Fix
    Signed-off-by: Guy Davidi <guydav1111@gmail.com>

---
+++ src/version.h
@@ -0,0 +1,20 @@
+#ifndef RAVN_VERSION_H
+#define RAVN_VERSION_H
+
+// Auto-generated version information
+// Generated on: 2025-09-10 20:18:34 UTC
+
+#define RAVN_VERSION_MAJOR 2025
+#define RAVN_VERSION_MINOR 9
+#define RAVN_VERSION_PATCH 10
+#define RAVN_VERSION_MAGIC 4
+#define RAVN_VERSION_STRING "20250910.4"
+#define RAVN_BUILD_DATE "2025-09-10"
+#define RAVN_BUILD_TIME "20:18:34"
+#define RAVN_COMMIT_SHA "64a43ba"
+
+// Version format: YYYYMMDD.MAGIC
+// - YYYYMMDD: Date when version was created
+// - MAGIC: Incremental number for same-day builds
+
+#endif // RAVN_VERSION_H
