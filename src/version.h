77048832d2a637fb566aaa49b7c993fb81826b66
GitHub Action <action@github.com>
2025-10-07 07:32:22 +0000

    Update version to 20251007.1 [skip ci]

--- src/version.h
+++ src/version.h
@@ -2,16 +2,16 @@
 #define RAVN_VERSION_H
 
 // Auto-generated version information
-// Generated on: 2025-10-06 07:33:02 UTC
+// Generated on: 2025-10-07 07:32:01 UTC
 
 #define RAVN_VERSION_MAJOR 2025
 #define RAVN_VERSION_MINOR 10
-#define RAVN_VERSION_PATCH 6
+#define RAVN_VERSION_PATCH 7
 #define RAVN_VERSION_MAGIC 3
-#define RAVN_VERSION_STRING "20251006.3"
-#define RAVN_BUILD_DATE "2025-10-06"
-#define RAVN_BUILD_TIME "07:33:02"
-#define RAVN_COMMIT_SHA "b321008"
+#define RAVN_VERSION_STRING "20251007.3"
+#define RAVN_BUILD_DATE "2025-10-07"
+#define RAVN_BUILD_TIME "07:32:01"
+#define RAVN_COMMIT_SHA "0246a15"
 
 // Version format: YYYYMMDD.MAGIC
 // - YYYYMMDD: Date when version was created
